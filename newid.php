6005e3592de7ca446f5faa7317e5aae79163f5d2
tokkuu <117136185+tokkuu@users.noreply.github.com>
2022-11-25 12:59:14 +0900

    newid&rogin

---
+++ newid.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <p>新規登録</p>
+    <form method="post">
+        <input type="text" class="usename" name="nuser" placeholder="名前" maxlength="255"><br>
+        <input type="email" class="emale" name="ngmale" placeholder="メールアドレス"><br>
+        <input type="password" class="mypass" name="npass" placeholder="パスワード15桁以内" maxlength="15"><br>
+        <button>登録</button>
+    </form>
+
+</body>
+
+</html>
