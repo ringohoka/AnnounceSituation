a73bd65cad0e9c956972baa2f5285a0a9eb5f5bf
tokkuu <117136185+tokkuu@users.noreply.github.com>
2022-12-09 10:09:22 +0900

    mein

---
+++ js/js_split.js
@@ -0,0 +1,11 @@
+{
+    $(window).on('load resize', function () {
+        var windowWidth = window.innerWidth;
+        var elements = $('#fixed-area');
+        if (windowWidth >= 768) {
+            Stickyfill.add(elements);
+        } else {
+            Stickyfill.remove(elements);
+        }
+    });
+}
--- php_grouplist.php
+++ php_grouplist.php
@@ -4,46 +4,61 @@
 <head>
     <title>グループ一覧</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/css_newgroup_grouplist.css">
-    <link rel="stylesheet" href="css/css_groupin_grouplist.css">
+    <!-- <link rel="stylesheet" href="css/css_newgroup_grouplist.css"><!-- グループ作成用 -->
+    <!-- <link rel="stylesheet" href="css/css_groupin_grouplist.css">グループ加入用 -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/6-4.css">
 </head>
 
 <body>
-    <h3>グループに加入するかグループを作成してください。</h3>
 
-    
-    <div class="situation">
-        <p><img src="image/赤●.png">取り込み中</p>
-        <p><img src="image/緑.png">退席中</p>
-        <p><img src="image/黄.png">会議中</p>
-    </div>
-    <div class="InOrNew">
-        <input type="submit" id="click-btn" value="グループの作成">
-        <div id="popup-wrapper">
-            <div id="popup-inside">
-                <div id="close">x</div>
-                <div id="message">
-                    <a href="#">作成</a>
-                </div>
+
+    <div id="wrapper">
+        <!--左エリア-->
+
+        <div id="container">
+            <div id="brid">
+                <?php for ($i = 1; $i <= 100; $i++) { ?>
+                    <section>
+                        <h2>Area<?php echo $i ?></h2>
+                        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+                    </section>
+                <?php } ?>
             </div>
         </div>
-        <input type="submit" id="click-btn2" value="グループの加入">
-        <div id="popup-wrapper2">
-            <div id="popup-inside2">
-                <div id="close2">x</div>
-                <div id="message2">
-                    <h2>グループ作成</h2>
-                    <input type="text" name="newgroup" placeholder="グループ名入力"><br>
-                    <a href="#">グループを作成</a>
-                    <a href="php_grouplist.php">キャンセル</a>
-                </div>
+
+        <!--右固定エリア-->
+        <div id="fixed-area">
+            <div id="stbt_list">
+                <form method="get">
+                    <button class="btred" type="submit" name="1">
+                        <i class="fa-solid fa-circle" style=color:red;></i>
+                        取り込み中
+                    </button><br>
+                    <button class="btgreen" type="submit" name="2">
+                        <i class="fa-solid fa-circle" style=color:green;></i>
+                        退席中
+                    </button><br>
+                    <button class="btyellow" type="submit" name="3">
+                        <i class="fa-solid fa-circle" style=color:yellow;></i>
+                        会議中
+                    </button><br>
+                </form>
             </div>
         </div>
-    </div>
-    
-   
-    <script src="js/js_groupin_grouplist2.js"></script>
-    <script src="js/js_newgroup_grouplist2.js"></script>
+
+
+
+        <!--/wrapper-->
+
+        <script src="https://kit.fontawesome.com/841b98f2a1.js" crossorigin="anonymous"></script>
+        <script src="js/js_groupin_grouplist2.js"></script>
+        <script src="js/js_newgroup_grouplist2.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/stickyfill/2.1.0/stickyfill.min.js"></script>
+        <script src="http://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/move02/6-4/js/6-4.js"></script>
+        <script src="js/js_split.js"></script>
+
 </body>
 
 </html>
