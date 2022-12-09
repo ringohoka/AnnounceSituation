c84cffa16a855c2c339273f229f258af0a74cb39
tokkuu <117136185+tokkuu@users.noreply.github.com>
2022-12-09 14:24:08 +0900

    rogin

--- newid.php
+++ newid.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/inputform.css">
     <title></title>
 </head>
 
@@ -14,29 +15,7 @@
         <input type="text" class="usename" name="nuser" placeholder="名前" maxlength="255"><br>
         <input type="email" class="emale" name="ngmale" placeholder="メールアドレス"><br>
         <input type="password" class="mypass" name="npass" placeholder="パスワード15桁以内" maxlength="15"><br>
-        <button onclick="php_grouplist">登録</button>
-
-
-
-
-        <?php //if($_POST['nuser']<0 && $_POST['ngmale']<0 && $_POST['npass']<0){ ?> <!-- 何もなし -->
-            <!-- <button>登録</button> -->
-        <?php //}else if ($_POST['nuser']<0){ ?> <!-- 名前がない -->
-            <?php //echo "名前がない"; ?>
-            <!-- <button>登録</button> -->
-        <?php //}else if ($_POST['ngmale']<0){?> <!-- メアドがない -->
-            <?php //echo "メアドがない"; ?>
-            <!-- <button>登録</button> -->
-        <?php //}else if ($_POST['npass']<0){?> <!-- パスがない -->
-            <?php //echo "パスがない"; ?>
-            <!-- <button>登録</button> -->
-        <?php //}else{?> <!-- すべて入力 -->
-            <?php //echo "OK!"; ?>
-            <!-- <button onclick="php_grouplist">登録</button> -->
-        <?php //}?>
-        <!-- echo $_POST['nuser']; -->
-        
-        
+        <button onclick="php_grouplist">登録</button>    
     </form>
     
     
